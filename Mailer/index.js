aa9d9214375491dbfa4165a5fc7b9cdea3e16233
Mpk21289489mpk <149400601+Mpk21289489mpk@users.noreply.github.com>
2023-11-17 18:01:53 +0300

    Update index.js

--- Mailer/index.js
+++ Mailer/index.js
@@ -19,17 +19,18 @@ class SendServiceEmail {
         return new Promise((resolve, reject) => {
             let mailOptions = {
                 from: process.env.SERVICE_EMAIL,
-                priority: "high",
                 to: params.recipient,
                 subject: params.subject,
                 text: `${params.text}\n\n\n\nЭто сообщение является системным. Пожалуйста, не отвечайте на него.`
             };
-            serviceMailTransporter.sendMail(mailOptions, function (err, info) {
-                   if(err)
-                     console.log(err)
-                   else
-                     console.log(info);
-            }).then((result) => { resolve(result); });
+            serviceMailTransporter.sendMail(mailOptions).then(
+                (result) => { 
+                    resolve(result); 
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
         });
     }
 }
